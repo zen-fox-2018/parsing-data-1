56218781654b4b5e3a79b101c35b364e334a78d9
kangman53 <hermanboy53@gmail.com>
2018-12-03 17:37:52 +0700

    herman yulianto parsing data 1 done

--- parser.js
+++ parser.js
@@ -4,53 +4,77 @@ class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
     constructor(obj){
-        this.id = parser.people.length;
+        this.id = obj.id;
         this.first_name = obj.first_name;
         this.last_name = obj.last_name;
         this.email = obj.email;
         this.phone = obj.phone;
-        this.created_at = JSON.stringify(new Date());
+        this.created_at = obj.created_at;
     }
 }
 
 class PersonParser {
 
-  constructor(file) {
-    this._file = file
-    this._people = fs.readFileSync(file, 'utf-8').split('\n');
-      //this._size = this._people.length;
-  }
+    constructor(file) {
+        this._file = file
+        this._people = [];
+        this.listPeople();
+    }
 
-  get people() {
-    return this._people;
-  }
+
+    get people() {
+        return this._people;
+    }
+
+    set people(obj) {
+        this._people.push(obj);
+    }
     
-  get file() {
-    return this._file;
-  }
-    
-    addPerson(obj) {
-        let newPerson = Object.values(obj).join(',');
-        this._people.push(newPerson);
+    get file() {
+        return this._file;
+    }
+
+    listPeople () {
+        let dataPeople = fs.readFileSync('./people.csv','utf-8').split('\n');
+        for (let i = 0 ;  i < dataPeople.length; i++) {
+            let person = dataPeople[i].split(',');
+            let objPerson = {
+                id : person[0],
+                first_name : person[1],
+                last_name : person[2],
+                email : person[3],
+                phone : person[4],
+                created_at : person[5]
+            };
+            this.people = new Person(objPerson);
+        }
+    }
+
+    addPerson(person) {
+        this.people = person;
     }
 
     save(){
-        fs.writeFileSync(this._file,this._people.join('\n'));
+        let dataSave = [];
+        this.people.forEach( e => {
+            dataSave.push(Object.values(e).join(','));
+        });
+        fs.writeFileSync(this._file,dataSave.join('\n'));
     }
 }
 
 let parser = new PersonParser('people.csv')
 
-//console.log(parser.people);
-
 let newPerson = {
+    id : parser.people.length,
     first_name : 'Herman',
     last_name : 'Yulianto',
     email : 'hermanboy53@gmail.com',
-    phone : '081213183958'
+    phone : '081213183958',
+    created_at : JSON.stringify(new Date())
 }
 
 parser.addPerson(new Person(newPerson));
-//console.log(parser.person);
+//console.log(parser.people);
 parser.save();
 console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
