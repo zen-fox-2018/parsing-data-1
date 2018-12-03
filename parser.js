3d9569758d2564d1b4c025cfbf5b3cfe7e911fab
ahmad <ahmad.syukron6991@gmail.com>
2018-12-03 21:22:11 +0700

    data parse

--- parser.js
+++ parser.js
@@ -1,6 +1,14 @@
 "use strict"
-
+const fs = require('fs')
 class Person {
+  constructor(id,firstName,lastName,email,phone,created){
+    this.id = id;
+    this.first_name = firstName;
+    this.last_name = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.created_at = created;
+  }
   // Look at the above CSV file
   // What attributes should a Person object have?
 }
@@ -9,17 +17,68 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = [] 
   }
 
-  get people() {
+  set people(input) {
+    this._people.push(input)
+  }
+  get people(){
     return this._people
   }
 
-  addPerson() {}
+  get file () {
+    return this._file
+  }
+
+  dataToArray(){
+    return fs.readFileSync(this.file, 'utf-8')
+  }
+
+  readFile () {
+    let dataArray =  this.dataToArray.split('\n')
+    dataArray =  dataArray.slice(1)
+    let splitted = ''
+    
+    for(let i = 0; i < dataArray.length; i++){
+        dataArray[i] =  dataArray[i].split(',')
+        splitted = new Person(dataArray[i][0],dataArray[i][1],dataArray[i][2],dataArray[i][3],dataArray[i][4],dataArray[i][5])
+        this.people =  splitted
+    }
+
+   return this.readFile
+  }
+
+  
+  addPerson(obj) {
+    this._people.push(obj)
+  }
+
+  save(){
+    let readyCovert = []
+    for(let i = 0; i < this.people.length; i++){
+      readyCovert.push(Object.values(this.people[i]).join(','))
+    }
+    fs.writeFileSync(this.file, readyCovert.join('\n'))
+  }
 
 }
 
-let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+
+let parser = new PersonParser('people.csv')
+// let ibenk = {
+//               firstName : 'ahmad',
+//               lastName : 'syukron',
+//               email : 'ahmad@gmail.com',
+//               phone : '082202545896'
+//             }
+
+
+console.log(parser.readFile())
+//console.log(parser)
+parser.addPerson(new Person(220,'ahmad','syukron', 'ahmd@SpeechGrammarList.com', '082202545986', new Date));
+console.log(parser.people)
+console.log(parser.save())
+console.log(`There are ${parser.people.length-1} people in the file '${parser.file}'.`)
+
